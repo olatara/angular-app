dc5df621256e9a8fd27aa652ca62a355b4cc61fd
Tara Bisi-Afolabi <olatara@taras-mbp.home>
2020-11-02 18:02:32 +0000

    added github token

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -170,8 +170,8 @@ resource "aws_codebuild_project" "convertr-codebuild" {
 #   }
 # }
 
-# resource "aws_codebuild_source_credential" "github_token" {
-#   auth_type   = "PERSONAL_ACCESS_TOKEN"
-#   server_type = "GITHUB"
-#   token       = var.github_token
-# }
+resource "aws_codebuild_source_credential" "github_token" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = var.github_token
+}
